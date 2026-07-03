e95d381e0d695566b693bcb674ba7996aee3c3a5
learnWithNishu <nishumhubscit@gmail.com>
2026-07-03 15:44:52 +0530

    Initial Commit

---
+++ src/main/java/com/resume/job/tracker/ResumeJobTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.resume.job.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeJobTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeJobTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resume/job/tracker/ResumeJobTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resume.job.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeJobTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
