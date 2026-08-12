26ba2d41ff886cc5a02c83c1404b89a94145dceb
Nishu <nishumca22@gmail.com>
2026-08-12 23:08:03 +0530

    feat(discovery): register tracker service with Eureka

--- src/main/java/com/resume/job/tracker/ResumeJobTrackerApplication.java
+++ src/main/java/com/resume/job/tracker/ResumeJobTrackerApplication.java
@@ -2,8 +2,10 @@ package com.resume.job.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ResumeJobTrackerApplication {
 
 	public static void main(String[] args) {
